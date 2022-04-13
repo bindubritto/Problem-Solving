67b2519b334d091ce44501188402e10c73370923
bindubritto <sabbir.ruet.cse>
2022-04-13 22:50:02 +0600

    Solved Single Number problem

---
+++ leetcode/week-01/136-Single-Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        
+        for(auto num : nums) {
+            res = res ^ num;
+        }
+        return res;
+    }
+};
