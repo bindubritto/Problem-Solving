7cd1ae499691c65db3c407fbc7ca8a17a24767c5
bindubritto <sabbir.ruet.cse>
2022-04-13 21:07:20 +0600

    Solution updated

--- leetcode/week-01/001-Two-Sum.cpp
+++ leetcode/week-01/001-Two-Sum.cpp
@@ -1,17 +1,35 @@
-// Optimal | O(n) space, O(n) time
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
 
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> seen;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            int potentialMatch = target - nums[i];
-            if(seen.count(potentialMatch)){
-                return { seen[potentialMatch], i};
+    public:
+
+        // Optimal | O(n) space, O(n) time
+        vector<int> twoSum(vector<int>nums, int target) {
+            unordered_map<int, int> seen;
+            
+            for (int i = 0; i < nums.size(); i++) {
+                int potentialMatch = target - nums[i];
+                if(seen.count(potentialMatch)){
+                    return { seen[potentialMatch], i};
+                }
+                seen[nums[i]] = i;
             }
-            seen[nums[i]] = i;
+            return {};
         }
-        return {};
-    }
 };
+
+int main() {
+
+    freopen("out.txt", "w", stdout);
+
+    Solution sln;
+
+    vector<int> res = sln.twoSum({2,7,11,15}, 9);
+
+    cout<<res[0]<<"\n";
+    cout<<res[1]<<"\n";
+}
